d8ed1d80e4364ea548d5ffd3f336a8bb1d9b4144
Ignacio Van Droogenbroeck <ignacio@vandroogenbroeck.net>
2021-10-23 22:57:45 -0500

    small changes

--- line-protocol-generator/main.py
+++ line-protocol-generator/main.py
@@ -1,4 +1,4 @@
-# line protocol data generator
+# line protocol data generator by Ignacio Van Droogenbroeck
 
 import random
 import time
@@ -8,11 +8,11 @@ tag_key = ["host"]
 tag = ["server01", "server02"]
 fields = ["load", "used"]
 values = random.sample(range(1, 100), 30)
+timestamp = int(time.time())
 
 writer = open("data.txt", "w")
 
 while True:
-    timestamp = int(time.time())
     for i in range(len(values)):
         line = "{0},{1}={2} {3}={4} {5}\n".format(measurement[0], tag_key[0], tag[0], fields[0], values[i], timestamp)
         writer.write(line)
